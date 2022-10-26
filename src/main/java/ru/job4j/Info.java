951f52bc103799e25598db94f5538a93a05a65ee
Владимир <bf.karatel@gmail.com>
2022-10-26 20:47:53 +0800

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,12 +2,11 @@ package ru.job4j;
 
 import java.util.Calendar;
 
-
 public class Info {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         final  Calendar currentDate = Calendar.getInstance();
-        System.out.println(currentDate.get(Calendar.DAY_OF_MONTH) +
-                "." + currentDate.get(Calendar.MONTH) +
-                "." + currentDate.get(Calendar.YEAR));
+        System.out.println(currentDate.get(Calendar.DAY_OF_MONTH)
+                + "." + currentDate.get(Calendar.MONTH)
+                + "." + currentDate.get(Calendar.YEAR));
     }
 }
