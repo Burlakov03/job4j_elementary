efc322e1f6488a7187193d45e2f4ce4876b79e76
Владимир <bf.karatel@gmail.com>
2022-10-30 13:08:37 +0800

    4.6.1. Switch [#334070 #356026]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -1,21 +1,39 @@
 package ru.job4j.condition;
 
 public class SwitchWeek {
-    public static String nameOfDay(int day) {
+    public static String numberOfDay(int day) {
+        String name;
         switch (day) {
-            case 1: return "Понедельник";
-            case 2: return "Вторник";
-            case 3: return "Среда";
-            case 4: return "Четверг";
-            case 5: return "Пятница";
-            case 6: return "Суббота";
-            case 7: return "Воскресенье";
-            default: return "Ошибка";
+            case 1:
+                name = "Понедельник";
+                break;
+            case 2:
+                name = "Вторник";
+                break;
+            case 3:
+                name = "Среда";
+                break;
+            case 4:
+                name = "Четверг";
+                break;
+            case 5:
+                name = "Пятница";
+                break;
+            case 6:
+                name = "Суббота";
+                break;
+            case 7:
+                name = "Воскресенье";
+                break;
+            default: name = "Ошибка";
         }
-
+        return name;
     }
 
     public static void main(String[] args) {
-        System.out.println(SwitchWeek.nameOfDay(5));
+        System.out.println(numberOfDay(5));
+        System.out.println(numberOfDay(8));
+        System.out.println(numberOfDay(7));
+        System.out.println(numberOfDay(1));
     }
 }
